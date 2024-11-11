282607a7c91d27a4be5ee714e8e61ef526cf7f19
coryrmrz <coryramirez2018@gmail.com>
2024-11-10 22:17:20 -0500

    Implement main application and log portfolio details and transactions.

---
+++ main.js
@@ -0,0 +1,14 @@
+//Task 4: Create the main application
+import { calculatePortfolioValue, getPortfolioAllocation } from "./portfolio.js"; //import formulas from portfolio.js
+import Transaction from "./transaction.js"; //import transaction class from transaction.js
+
+console.log("Total Portfolio Value:", calculatePortfolioValue()); //displays total portfolio value output
+console.log("Allocation Percentages:", getPortfolioAllocation()); //displays allocation percentages output
+
+const transaction1 = new Transaction(1, "sell", 10); // selling 10 stock shares of AMZN - Amazon
+console.log("First Transaction:", transaction1); //displays first transaction output
+const transaction2 = new Transaction(3, "buy", 20); //buying 20 stock shares of JPM - JP Morgan
+console.log("Second Transaction:", transaction2); //displays second transaction output
+
+console.log("Updated Total Portfolio Value:", calculatePortfolioValue()); //displays updated portfolio value output
+console.log("Updated Allocation Percentages:", getPortfolioAllocation()); //displays updated allocation percentages output
